9d6840172c1c8eacca64f68befbef4ed63e63395
SantiBechir <santiagobechir05@gmail.com>
2025-09-24 20:08:26 -0300

    Creación DestinoDto y creación DestinoAppService

---
+++ TravelBuddy/src/TravelBuddy.Application.Contracts/Destinos/DestinoDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBuddy.Destinos
+{
+    public class DestinoDto
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Pais { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+    public class CreateDestinoDto
+    {
+        public string Nombre { get; set; }
+        public string Pais { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
